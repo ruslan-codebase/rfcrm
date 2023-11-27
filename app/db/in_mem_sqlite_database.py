26932bf010e92f231f90fd421d2eff1aba08ebec
Ruslan Filimonov <ruslan.filimonov.main@mail.ru>
2023-11-27 02:07:08 +0200

    Add in-memory database for testing

---
+++ app/db/in_mem_sqlite_database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from app.db.base_database import Database
+
+
+class InMemSqliteDatabase(Database):
+
+    def setup(self) -> None:
+        sqliteurl = "sqlite+aiosqlite://"
+        self.async_engine = create_async_engine(sqliteurl)
+        self.async_sessionmaker = sessionmaker(
+            bind=self.async_engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
