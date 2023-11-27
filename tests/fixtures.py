3a889bdebeb6bbc46573d5b9636413061288ac5a
Ruslan Filimonov <ruslan.filimonov.main@mail.ru>
2023-11-27 02:10:06 +0200

    Add test fixtures

---
+++ tests/fixtures.py
@@ -0,0 +1,24 @@
+from pytest import fixture
+from httpx import AsyncClient
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.in_mem_sqlite_database import InMemSqliteDatabase
+from app.db import db
+from app.main import app
+
+
+test_db = InMemSqliteDatabase()
+
+@fixture(scope="function")
+async def async_session() -> AsyncGenerator[AsyncSession, None]:
+    await test_db.init_db()
+    yield test_db.get_session()
+    await test_db.destroy_db()
+
+
+@fixture(scope="function")
+async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    await test_db.init_db()
+    app.dependency_overrides[db.get_session] = test_db.get_session
+    async with AsyncClient(app=app, base_url="http://testfastapi.localhost") as client:
+        yield client
